a79797cdaf3486d2ba12d3c58e09071325050cfb
Christian Torres <mannyt5889@gmail.com>
2026-03-13 14:34:54 -0400

    Created Diet Module with widgets

---
+++ HealthingHand.Web/Services/DietItems/DietMealItemInput.cs
@@ -0,0 +1,12 @@
+namespace HealthingHand.Web.Services.DietItems;
+
+public class DietMealItemInput
+{
+    public string Name { get; set; } = "";
+    public float Quantity { get; set; } = 1;
+    public string Unit { get; set; } = "serving";
+    public int Calories { get; set; }
+    public float ProteinGrams { get; set; }
+    public float CarbsGrams { get; set; }
+    public float FatGrams { get; set; }
+}
